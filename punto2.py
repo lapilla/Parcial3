e028fe6664f4a2398b448163ec7396b2770a521d
Lau <laura.castiblanco2@correo.usa.edu.co>
2019-11-18 19:22:59 -0500

    segundo punto

--- punto2.py
+++ punto2.py
@@ -7,22 +7,12 @@ from tkinter import *
 from PIL import Image
 from PIL import ImageTk
 import time
-cont=0
-cont1=100
-prom=0
 
 placa = Arduino ('COM3')
 it = util.Iterator(placa)
 it.start()
-a_0 = placa.get_pin('a:0:i')
-a_1 = placa.get_pin('a:1:i')
-a_2 = placa.get_pin('a:2:i')
-led = placa.get_pin('d:8:o')
+
 led1 = placa.get_pin('d:9:p')
-led5 = placa.get_pin('d:10:p')
-led6 = placa.get_pin('d:11:p')
-led7 = placa.get_pin('d:12:o')
-led8 = placa.get_pin('d:13:o')
 
 time.sleep(0.1)
 ventana = Tk()
@@ -47,67 +37,44 @@ pot2=StringVar()
 valor2= Label(frame1, bg='cadet blue', font=("Arial Bold", 15), fg="white", width=5)
 pot3=StringVar()
 
-
-
 def pot_A0():
-    global prom
-    i=1
-    prom=0
-    x=a_0.read()
-    print(x)
-    pot1.set(x)
-    ventana.update()
-    time.sleep(0.1)
-        
-    ref = db.reference('sensor')
-    ref.update({
-            
-        'sensor1/pot1': x
-    })
+    ref = db.reference('sensor/sensor1')
+    x=ref.get()  
+    y=x.get('pot1')
+    print('pot1:',y)
+    led1.write(y)
+
 
 def pot_A1():
-    global prom
-    i=0
-    prom=0
-    x=a_1.read()
-    print(x)
-    pot2.set(x)
-    ventana.update()
-    time.sleep(0.1)
-    ref = db.reference('sensor')
-    ref.update({
-        'sensor1/pot2': x
-    })
-   
+    ref = db.reference('sensor/sensor1')
+    x=ref.get()  
+    y=x.get('pot2')
+    print('pot2:',y)
+    led1.write(y)
+
 
 def pot_A2():
-    global prom
-    i=0
-    prom=0
-    x=a_2.read()
-    print(x)
-    pot3.set(x)
-    ventana.update()
-    time.sleep(0.1)
-    ref = db.reference('sensor')
-    ref.update({
-        'sensor1/pot3': x
-    })
+    ref = db.reference('sensor/sensor1')
+    x=ref.get()  
+    y=x.get('pot3')
+    print('pot2:',y)
+    led1.write(y)
+
 
 valor.configure(textvariable=pot1)
 valor.place(x=20, y=30)
-bot1=Button(text="pot_A0",command=pot_A0)
-bot1.place(x=120, y=36)
+adc1_update=Button(text="pot_A0",command=pot_A0)
+adc1_update.place(x=120, y=36)
 
 valor2.configure(textvariable=pot2)
 valor2.place(x=20, y=60)
-bot2=Button(text="pot_A1",command=pot_A1)
-bot2.place(x=120, y=70)
+adc2_update=Button(text="pot_A1",command=pot_A1)
+adc2_update.place(x=120, y=70)
 
 valor1.configure(textvariable=pot3)
 valor1.place(x=20, y=90)
-bot3=Button(frame1,text="pot_A2",command=pot_A2)
-bot3.place(x=114, y=94)
+adc3_update=Button(frame1,text="pot_A2",command=pot_A2)
+adc3_update.place(x=114, y=94)
 
 
 ventana.mainloop()
